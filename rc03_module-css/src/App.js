fc15f056182f82e5e6f29d3f216e42d1923d8661
ÇETİN <cetinnnarasss@gmail.com>
2022-06-23 21:33:34 +0300

    finish 3

--- rc03_module-css/src/App.js
+++ rc03_module-css/src/App.js
@@ -1,7 +1,21 @@
+import Card from "./components/card/Card";
+import data from "./util/data";
+
 function App() {
+  //? console.log(data);
   return (
     <>
-      <h1>APP</h1>
+    {data.map((item) => {
+      //!Destruction(ayırma) ,,map = dolaşma
+      const {id, img, btnName, dil} = item;
+      
+      return <Card key={id} img={img} btnName={btnName} dil={dil}/>
+      //key burda uniq bir değer ver ki hata vermesin.
+      //id yerine idex de kullanılabılır ama ıd kullanalım(mapde index kullanabılırsın)
+
+    
+    })}
+      
     </>
   );
 }
